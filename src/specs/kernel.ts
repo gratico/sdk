4aadd2f931f570b5a0d27e3160c065f44e9960aa
Abhishiv Saxena <abhishiv@gmail.com>
2022-04-12 11:49:18 +0300

    builtins

--- src/specs/kernel.ts
+++ src/specs/kernel.ts
@@ -87,7 +87,7 @@ export interface IKernelExtensionMeta {
 export interface IKernelExtensionAdvertisment {
 	id: string;
 	meta: IKernelExtensionMeta;
-	capabilities: string[];
+	capability: string;
 	moduleLocation?: any;
 	exportName?: string;
 }
