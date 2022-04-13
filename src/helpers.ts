ecd82ccaf99171251c4f875c8dc0848e487dffc8
Abhishiv Saxena <abhishiv@gmail.com>
2022-04-13 17:54:58 +0300

    restore messages Makefile

---
+++ src/helpers.ts
@@ -0,0 +1,29 @@
+import { IKernel, IKernelExtensionAdvertisment } from "./specs";
+export async function resolveModule(
+	kernel: IKernel,
+	pkgName: string,
+	advt: IKernelExtensionAdvertisment
+) {
+	const mod = await kernel.pm.getModule("pkgName");
+	return mod[advt.exportName || "__default"];
+}
+export async function getExtension(kernel: IKernel, capability: string) {
+	const list = kernel.extensions[capability];
+	if (list && list.length > 0) {
+		const item = list[0];
+		return resolveModule(kernel, item.pkgName, item.advt);
+	}
+}
+
+export async function getExtensions(kernel: IKernel, capability: string) {
+	const list = kernel.extensions[capability];
+	if (list && list.length > 0) {
+		return Promise.all(
+			list
+				.map(async (item) => {
+					return resolveModule(kernel, item.pkgName, item.advt);
+				})
+				.filter((el) => el)
+		);
+	}
+}
