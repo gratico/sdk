9c4d3572a1645afa648808eefa4ff3809e52f0f6
Abhishiv Saxena <abhishiv@gmail.com>
2022-04-23 00:01:11 +0300

    change sdk

--- src/specs/kernel.ts
+++ src/specs/kernel.ts
@@ -48,71 +48,27 @@ export type FileNameDependentIcon = {
 };
 export type Icon = NPMHostedIcon | RawIcon | FileNameDependentIcon;
 
+export type TreeNodeType = "root" | "dir" | "file" | "submodule";
+
 export type DBRecord<T = any> = {
 	id: string;
 	rev: string;
 	parentId: string;
 	ancestorIds: string[];
-	score: string;
-	type: string;
+	type: TreeNodeType;
 	name: string;
+	score?: string;
 	applicationId?: string;
 	icon?: Icon;
-	payload: T;
 };
 
-export type DBNode<T = any> = {
-	type: "db";
-	id: string;
-	parentId: string;
-	score: string;
-	rev: string;
-	name: string;
-	attrs: DBRecord<T>;
-	expandable?: boolean;
-	state: Record<string, any>;
-};
-export type FileNode<T = any> = {
-	type: "file";
-	id: string;
-	parentId: string;
-	score: string;
-	name: string;
-	expandable?: boolean;
-	attrs: DBRecord<T>;
-	state: Record<string, any>;
-};
-export type DirNode<T = any> = {
-	type: "dir";
-	id: string;
-	parentId: string;
-	name: string;
-	score: string;
-	attrs: DBRecord<T>;
-	state: Record<string, any>;
-};
-export type SubmoduleNode<T = any> = {
-	type: "submodule";
-	id: string;
-	parentId: string;
-	name: string;
-	score: string;
-	attrs: DBRecord<T>;
-	state: Record<string, any>;
-};
-export type RootNode = {
+export type TreeNode<T = any> = {
 	type: "root";
 	id: string;
 	name: string;
+	attrs: DBRecord<T>;
 	state: Record<string, any>;
 };
-export type TreeNode<T = any> =
-	| DBNode<T>
-	| FileNode<T>
-	| DirNode<T>
-	| SubmoduleNode<T>
-	| RootNode;
-
 export interface RootChildrenFetcher {
 	type: string;
 	fetchChildren: (parentNode: TreeNode) => Promise<TreeNode[]>;
